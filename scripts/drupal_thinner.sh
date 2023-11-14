72cd77b20f35e9a316f1a24043f91e37d8c07fd1
Matthieu <m@matthieuscarset.com>
2023-11-14 11:33:36 +0100

    feat: 🍃Add eco-friendly script🍃

--- scripts/drupal_thinner.sh
+++ scripts/drupal_thinner.sh
@@ -12,10 +12,6 @@
 # Delete Drupal test directories.
 # https://unix.stackexchange.com/a/89937
 find . -type d -name tests -exec rm -rf {} +
-# Delete testing and demo profiles.
-find web/core/profiles -type d -name 'testing*' -exec rm -rf {} +
-find web/core/profiles -type d -name 'nightwatch_testing' -exec rm -rf {} +
-find web/core/profiles -type d -name 'demo_umami' -exec rm -rf {} +
 # Delete README files.
 find . -type f -name 'README*' -delete
 find . -type f -name 'CHANGELOG*' -delete
@@ -25,3 +21,7 @@ find . -type f -name 'PATCHES.txt' -delete
 find web -type f -name "*.md" -not -name "LICENSE*" -delete
 # Delete txt files except humans and licenses.
 find web -type f -name "*.txt" -not -name "salt*" -not -name "humans*" -not -name "LICENSE*" -not -name "COPYRIGHT*" -delete
+# Delete testing and demo profiles.
+find web/core/profiles -type d -name 'testing*' -exec rm -rf {} +
+find web/core/profiles -type d -name 'nightwatch_testing' -exec rm -rf {} +
+find web/core/profiles -type d -name 'demo_umami' -exec rm -rf {} +
